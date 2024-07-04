45d5df4076b7b22bd8929d64f0dfb5490c1acf08
VidhiShah02 <68293278+VidhiShah02@users.noreply.github.com>
2024-07-04 11:30:10 +0530

    Update artifact.tf

--- terraform/artifact.tf
+++ terraform/artifact.tf
@@ -1,9 +1,4 @@
-# resource "google_artifact_registry_repository" "python-app" {
-#   location = var.region
-#   repository_id = "python-app"
-#   description = "Python App Repository"
-#   format = "DOCKER"
-# }
+
 resource "google_artifact_registry_repository" "python-app" {
   location = var.region
   repository_id = var.registery_name
